317850ba40f605610878dc9485d3469b644757b8
Manoj.Gali <manoj.gali.ctr@sagesure.com>
2026-09-15 07:57:11 +0530

    Created Config Manager class for Managing the multiple environments {dev, qa, uat} for the project.

--- src/test/java/com/api/utils/ConfigManager.java
+++ src/test/java/com/api/utils/ConfigManager.java
@@ -5,16 +5,26 @@ import java.util.Properties;
 
 public class ConfigManager {
     private static Properties prop = new Properties();
-    private static String path ="config/config.properties";
+    private static String path = "config/config.properties";
+    private static String env;
 
-    private ConfigManager(){
+    private ConfigManager() {
 
     }
 
     static {
+        env = System.getProperty("env", "qa");
+        env = env.trim().toLowerCase();
+        System.out.println("Running tests in Env: " + env);
+        switch (env) {
+            case "dev" -> path = "config/config.dev.properties";
+            case "qa" -> path = "config/config.qa.properties";
+            case "uat" -> path = "config/config.uat.properties";
+            default -> path = "config/config.qa.properties";
+        }
         InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
-        if(path==null){
-            throw new RuntimeException("Cannot find the file path: "+path);
+        if (path == null) {
+            throw new RuntimeException("Cannot find the file path: " + path);
         }
         try {
             prop.load(input);
@@ -25,7 +35,7 @@ public class ConfigManager {
         }
     }
 
-    public static String getProperty(String key){
+    public static String getProperty(String key) {
         return prop.getProperty(key);
     }
 }
